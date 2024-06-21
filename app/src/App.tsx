3aa91c748db6999123dcc0c83817b07c0b99ade8
Kevin Yuan <kev.yuan01@gmail.com>
2024-06-21 16:19:10 -0700

    Added landing page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
 import type { Component } from 'solid-js';
+import Landing from './pages/Landing';
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <Landing></Landing>
   );
 };
 
