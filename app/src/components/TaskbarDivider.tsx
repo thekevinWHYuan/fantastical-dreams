79fe08cf11f3d4956aca7938a4fbfbd83ec45f95
Kevin Yuan <kev.yuan01@gmail.com>
2024-06-26 19:54:16 -0700

    minor aesthetic details and added windows screens for opening apps

---
+++ app/src/components/TaskbarDivider.tsx
@@ -0,0 +1,5 @@
+function TaskbarDivider(){
+    return (<div class="h-5/6 w-[0.5%] border-solid border-[3px] border-l-purple-300 border-pink-100 ml-1" />)
+}
+
+export default TaskbarDivider   
--- app/src/components/WindowsScreen.tsx
+++ app/src/components/WindowsScreen.tsx
@@ -1,5 +1,7 @@
 import { Component } from "solid-js";
 import WindowApp from "./WindowApps";
+import AppScreen from "./AppScreen";
+import TaskbarDivider from "./TaskbarDivider";
 
 const WindowsScreen: Component = () => {
     
@@ -15,12 +17,16 @@ const WindowsScreen: Component = () => {
 
         <div class="h-[94%] w-full grid grid-cols-8 grid-rows-8 bg-windows-desktop grid-flow-col p-10">
             <WindowApp source="src/public/images/resume_app.png" name="Resume"/>
+            <AppScreen></AppScreen>
         </div>
+
         <nav class="w-full h-[6%] flex items-center pl-2 pr-2 bg-[#f6e0f7]">    
             <button class="w-[15%] shadow-md h-3/5 hover:cursor-cute-pointer flex shadow-purple-100 border-solid border-purple-400 border-[3px] border-t-pink-100 border-l-pink-100 text-2xl">
                 <img src="src/public/images/custom_windows_logo.png" class="h-full object-contain aspect-square"/>
                 <p class="object-fill w-full h-full flex items-center justify-center">Start</p>
-            </button>   
+            </button>
+            <TaskbarDivider/>
+            <TaskbarDivider/>
         </nav>
     </div>);
 }   
