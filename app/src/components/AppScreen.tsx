7c6485d597d5d50814059d22f664f1643bbaa7ef
Kevin Yuan <kev.yuan01@gmail.com>
2024-06-26 19:55:20 -0700

    created windows screens for opening apps

---
+++ app/src/components/AppScreen.tsx
@@ -0,0 +1,13 @@
+function AppScreen(){
+    return(<div class="aspect-[4/3] absolute w-1/3 border-2 border-purple-500 p-2">
+        <nav class="w-full h-[5%] border-2 border-purple-500 flex justify-end">
+            <button class="aspect-square hover:cursor-cute-pointer">X</button>
+            <button class="aspect-square hover:cursor-cute-pointer">_</button>
+        </nav>
+        <section class="border-purple-500 border-2 border-solid w-full h-[90%] mt-2">
+
+        </section>
+    </div>)
+}
+
+export default AppScreen
