3476e541741df3237db710357fc42498df9121b1
Kevin Yuan <kev.yuan01@gmail.com>
2024-07-30 14:33:07 -0700

    added animations when opening and closing

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
       cursor: {
         "cute-cursor": "url('/src/public/nso_cursor.cur'), pointer",
         "cute-pointer": "url('/src/public/nso_point.cur'), pointer"
+      },
+      keyframes: {
+        "app-open": {
+            "0%": {transform: "scale(0.1)"},
+            "100%": {transform: "scale(1)"}
+        }
+      },
+      animation: {  
+        "app-open": "app-open 0.1s ease-in-out"
       }
     },
   },
