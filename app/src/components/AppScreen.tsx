190b74edc42cb817b9469030a84fa9161b09de19
Kevin Yuan <kev.yuan01@gmail.com>
2024-07-30 14:32:59 -0700

    used windowsApp context

--- app/src/components/AppScreen.tsx
+++ app/src/components/AppScreen.tsx
@@ -1,13 +1,28 @@
-function AppScreen(){
-    return(<div class="aspect-[4/3] absolute w-1/3 border-2 border-purple-500 p-2">
-        <nav class="w-full h-[5%] border-2 border-purple-500 flex justify-end">
-            <button class="aspect-square hover:cursor-cute-pointer">X</button>
-            <button class="aspect-square hover:cursor-cute-pointer">_</button>
-        </nav>
-        <section class="border-purple-500 border-2 border-solid w-full h-[90%] mt-2">
+import { Show } from "solid-js";
+import { useWindowsContext } from "../context/WindowsContext";
 
-        </section>
-    </div>)
+function AppScreen(props){
+
+    const { store, setStore } = useWindowsContext();
+
+    function closeWindow(){
+        setStore("windowApp", "")
+    }
+
+    return(
+        <Show when={store.windowApp != ""}>
+            <div class="aspect-[4/3] absolute w-1/3 border-2 border-purple-500 p-2 animate-app-open origin-top-left">
+                <nav class="w-full h-[5%] border-2 border-purple-500 flex justify-end">
+                    <button class="aspect-square hover:cursor-cute-pointer" onClick={closeWindow}>X</button>
+                    <button class="aspect-square hover:cursor-cute-pointer">_</button>
+                </nav>
+                <section class="border-purple-500 border-2 border-solid w-full h-[90%] mt-2">
+                    
+                </section>
+            </div>
+        </Show>
+
+)
 }
 
 export default AppScreen
