5d554935c51590266465e0d3fed80ad113d31d57
Kevin Yuan <kev.yuan01@gmail.com>
2024-08-02 10:51:17 -0700

    creating about me page

--- app/src/components/AppScreen.tsx
+++ app/src/components/AppScreen.tsx
@@ -1,7 +1,8 @@
 import { Show } from "solid-js";
 import { useWindowsContext } from "../context/WindowsContext";
+import Me from "./Me";
 
-function    AppScreen(props){
+function AppScreen(props){
 
     const { store, setStore } = useWindowsContext();
 
@@ -12,11 +13,11 @@ function    AppScreen(props){
     return(
         <Show when={store.windowApp != ""}>
             <div class="aspect-square absolute h-full border-2 border-purple-500 p-2 animate-app-open origin-top-left bg-gray-100">
-                <nav class="w-full h-[5%] border-2 border-purple-500 flex justify-end items-center bg-gradient-to-r from-gray-300 via-gray-300 to-gray-100">
-                    <button class="aspect-square hover:cursor-cute-pointer h-2/3 shadow-lgz border-2 text-2xl border-t-white border-l-white border-b-gray-300 border-r-gray-300 m-px" onClick={closeWindow}>X</button>
+                <nav class="w-full h-[5%] flex justify-end items-center bg-gradient-to-r from-gray-300 via-gray-300 to-gray-100">
+                    <button class="aspect-square hover:cursor-cute-pointer h-2/3 shadow-lgz border-2 text-2xl border-t-white border-l-white border-b-gray-300 border-r-gray-300 m-px flex items-center justify-center" onClick={closeWindow}>X</button>
                 </nav>  
-                <section class="border-purple-500 border-2 border-solid w-full h-[93%] mt-2">
-                    
+                <section class="border-gray-200 border-r-gray-50 border-b-gray-50 border-4 border-solid w-full h-[93%] mt-2">
+                    <Me/>
                 </section>
             </div>
         </Show>
