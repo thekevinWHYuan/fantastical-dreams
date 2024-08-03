00af77fac6666704bf880bc7c1aea0ad5154dc01
Kevin <kev.yuan01@gmail.com>
2024-08-03 14:12:01 -0700

    added posts for retro social media look

--- app/src/components/Me.tsx
+++ app/src/components/Me.tsx
@@ -1,13 +1,16 @@
+import Post from "./Posts";
+
 export default function Me(){
     return (<div class="grid grid-rows-10 grid-cols-10 h-full w-full p-2">
         <header class="col-span-10 row-span-2 flex bg-[url('src/public/images/mebanner.png')] items-center justify-center border-2 border-black bg-cover">
             <h1 class="text-[8vh]">About Me</h1>
         </header>
-        <section class="col-span-8 row-span-8">
-
-        </section>
         <aside class="col-span-2 row-span-8">
             
         </aside>
+        <section class="col-span-8 row-span-8">
+            <Post/>
+        </section>
+
     </div>);
 }
