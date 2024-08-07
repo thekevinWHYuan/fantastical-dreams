ab6b042a5abee1a977e274ead4613c039efb0b61
Kevin Yuan <kev.yuan01@gmail.com>
2024-08-07 11:57:03 -0700

    created about me page

--- app/src/components/Me.tsx
+++ app/src/components/Me.tsx
@@ -1,15 +1,33 @@
+import { useWindowsContext } from "../context/WindowsContext";
 import Post from "./Posts";
 
 export default function Me(){
+
+    const { setStore } = useWindowsContext();
+
     return (<div class="grid grid-rows-10 grid-cols-10 h-full w-full p-2">
         <header class="col-span-10 row-span-2 flex bg-[url('src/public/images/mebanner.png')] items-center justify-center border-2 border-black bg-cover">
             <h1 class="text-[8vh]">About Me</h1>
         </header>
-        <aside class="col-span-2 row-span-8">
-            
+        <aside class="col-span-2 row-span-8 flex flex-col pt-2">
+            <img src="src/public/images/resume_app.png" class="aspect-square"/>
+            <nav class="flex-grow flex flex-col">
+                <h2 class="bg-red-100 w-full h-1/6 flex items-center justify-center border-2 border-black">
+                    Related Items
+                </h2>
+                <ul class="flex-grow flex flex-col justify-around items-center">
+                    <li><a onClick={() =>setStore("windowApp", "About Me")}>About Me</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Resume")}>Resume</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Projects")}>Projects</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Endorsements")}>Endorsements</a></li>
+                </ul>
+            </nav>
         </aside>
-        <section class="col-span-8 row-span-8">
-            <Post/>
+        <section class="col-span-8 row-span-8 flex flex-col pl-2 pt-2">
+            <Post content="Kevin's a 3rd Year at Cal Poly studying Software Engineering and minoring in the Computing for the Interactive Arts program"/>
+            <Post content="Be sure to check out his other shenanigans and projects! Interests Include: OS, VR Development including 3D Modeling, Web Development"/>
+            <Post content="Look at what others have to say about him! He's sure to provide a valuable asset to your team!"/>
+
         </section>
 
     </div>);
--- app/src/components/Posts.tsx
+++ app/src/components/Posts.tsx
@@ -1,5 +1,17 @@
-export default function Post(){
-    return (<article>
-        
-    </article>);
+interface IPost{
+    content: string;
 }
+
+export default function Post(props: IPost){
+    return (<section class="w-full h-1/3 p-2 flex flex-row mt-5 mb-5">
+        <img src="#" class="aspect-square h-1/4"/>
+        <article class="h-full flex-grow flex border-2 flex-col">
+            <span class="w-full h-3/4 text-[1.6vh]">{props.content}</span>
+            <div class="w-full h-1/3 p-1">
+                <div class="border-2 w-3/4 h-full">
+                    <span class="text-[1.6vh] w-fit h-fit">Comment...</span>
+                </div>
+            </div>
+        </article>
+    </section>);
+}   
