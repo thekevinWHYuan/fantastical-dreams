19ab67c9c49a8238e5eae69c14e04ecada98822e
Kevin Yuan <kev.yuan01@gmail.com>
2024-08-07 11:57:44 -0700

    animations, background images

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -9,7 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage:{
-        "windows-desktop": "url('/src/public/windowsScreen.png')"
+        "windows-desktop": "url('/src/public/windowsScreen.png')",
+        "mini-scanline": "repeating-linear-gradient(to bottom, gray 0px, gray 2px, white 2px, white 4px)"
       },
       cursor: {
         "cute-cursor": "url('/src/public/nso_cursor.cur'), pointer",
@@ -19,10 +20,42 @@ const config: Config = {
         "app-open": {
             "0%": {transform: "scale(0.1)"},
             "100%": {transform: "scale(1)"}
-        }
+        },
+        "flicker":{
+          "0%": {opacity: "0.67861"},
+          "5%": {opacity: "0.74769"},
+          "10%": {opacity: "0.63604"},
+          "15%": {opacity: "0.70626"},
+          "20%": {opacity: "0.88128"},
+          "25%": {opacity: "0.63891"},
+          "30%": {opacity: "0.95583"},
+          "35%": {opacity: "0.57807"},
+          "40%": {opacity: "0.66559"},
+          "45%": {opacity: "0.84693"},
+          "50%": {opacity: "0.96019"},
+          "55%": {opacity: "0.88594"},
+          "60%": {opacity: "0.50313"},
+          "65%": {opacity: "0.71988"},
+          "70%": {opacity: "0.53455"},
+          "75%": {opacity: "0.67288"},
+          "80%": {opacity: "0.71428"},
+          "85%": {opacity: "0.70419"},
+          "90%": {opacity: "0.7003"},
+          "95%": {opacity: "0.86108"},
+          "100%": {opacity: "0.94387"}
+        },
+        "scanline": {
+          '0%': {transform: "translateY(calc(-50dvh))"},
+          '100%': {transform: "translateY(calc(99vh))"},
+        },
       },
       animation: {  
-        "app-open": "app-open 0.1s ease-in-out"
+        "app-open": "app-open 0.1s ease-in-out",
+        "flicker": "flicker infinite 0.15s",
+        "scanline": "scanline 2s linear infinite",
+      },
+      colors: {
+        "transparency": "rgba(76, 175, 80, 0.3)"
       }
     },
   },
