d7c36aae584e318e5d13599b5f0a4c7d6a869428
Kevin Yuan <kev.yuan01@gmail.com>
2024-08-17 08:23:13 -0700

    minor adjustment

--- app/src/components/DayStats.tsx
+++ app/src/components/DayStats.tsx
@@ -3,6 +3,6 @@ export function DayStats(){
     const today = new Date();
 
     return(<section class="border-2 w-[12%] h-3/5 border-l-purple-400 border-t-purple-400 border-b-pink-100 border-r-pink-100 flex items-center justify-center">
-        <p>{`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`}</p>
+        <span class="text-[100%]">{`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`}</span>
     </section>)
 }
