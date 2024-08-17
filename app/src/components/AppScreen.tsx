651ec0ea913597df727b6554e14d2d403ebde620
Kevin Yuan <kev.yuan01@gmail.com>
2024-08-17 08:23:41 -0700

    changed formatting for about me page

--- app/src/components/AppScreen.tsx
+++ app/src/components/AppScreen.tsx
@@ -1,6 +1,7 @@
 import { Show } from "solid-js";
 import { useWindowsContext } from "../context/WindowsContext";
 import Me from "./Me";
+import Touhou from "./Touhou";
 
 function AppScreen(props){
 
@@ -10,9 +11,9 @@ function AppScreen(props){
         setStore("windowApp", "")
     }
 
-    return(
+    return(     
         <Show when={store.windowApp != ""}>
-            <div class="aspect-square h-[85%] absolute border-2 border-[#bd98e0] p-2 pb-4 animate-app-open origin-top-left bg-gray-100 inset-0 m-auto flex flex-col">
+            <div class="aspect-square h-[85%] min-h-[85svh] absolute border-2 border-palette-light-purple p-2 pb-4 animate-app-open origin-top-left bg-gray-100 inset-0 m-auto flex flex-col overflow-hidden">
                 <nav class="w-full h-[4%] flex justify-end items-center bg-gradient-to-r from-gray-300 via-gray-300 to-gray-100">
                     <button class="aspect-square hover:cursor-cute-pointer h-2/3 shadow-lgz border-2 text-2xl border-t-white border-l-white border-b-gray-300 border-r-gray-300 m-px flex items-center justify-center" onClick={closeWindow}>X</button>
                 </nav>
@@ -24,12 +25,7 @@ function AppScreen(props){
                     </div>
                 </div>
                 <section class="border-gray-200 border-r-gray-50 border-b-gray-50 border-4 border-solid w-full flex-grow">
-                    <Show when={store.windowApp == "About Me"}>
-                        <Me/>
-                    </Show>
-                    <Show when={store.windowApp == "Resume"}>
-                        <iframe src="#" class="h-full w-full"></iframe>
-                    </Show>
+                    <Me banner="bg-[url('src/public/images/about_me_banner.png')]" title={store.windowApp} mode={store.windowApp}/>
                 </section>
             </div>
         </Show>
--- app/src/components/Me.tsx
+++ app/src/components/Me.tsx
@@ -1,33 +1,41 @@
+import { Show } from "solid-js";
 import { useWindowsContext } from "../context/WindowsContext";
 import Post from "./Posts";
 
-export default function Me(){
+interface IAboutMe{
+    title: string
+    banner: string
+    mode: string
+}
 
-    const { setStore } = useWindowsContext();
+export default function Me(props: IAboutMe){
+
+    const { store, setStore } = useWindowsContext();
 
     return (<div class="grid grid-rows-10 grid-cols-10 h-full w-full p-2">
-        <header class="col-span-10 row-span-2 flex bg-[url('src/public/images/mebanner.png')] items-center justify-center border-2 border-black bg-cover">
-            <h1 class="text-[8vh]">About Me</h1>
+        <header class={`col-span-10 row-span-2 flex ${props.banner} items-center justify-center border-2 border-palette-purple bg-cover`}>
+            <h1 class="text-[8vh] text-palette-purple">{props.title}</h1>
         </header>
         <aside class="col-span-2 row-span-8 flex flex-col pt-2">
-            <img src="src/public/images/resume_app.png" class="aspect-square"/>
+            <img src="src/public/images/temppfp.jpg" class="aspect-square"/>
             <nav class="flex-grow flex flex-col">
-                <h2 class="bg-red-100 w-full h-1/6 flex items-center justify-center border-2 border-black">
+                <h2 class="text-palette-purple w-full h-1/6 flex items-center justify-center border-2 border-palette-purple">
                     Related Items
                 </h2>
                 <ul class="flex-grow flex flex-col justify-around items-center">
-                    <li><a onClick={() =>setStore("windowApp", "About Me")}>About Me</a></li>
-                    <li><a onClick={() =>setStore("windowApp", "Resume")}>Resume</a></li>
-                    <li><a onClick={() =>setStore("windowApp", "Projects")}>Projects</a></li>
-                    <li><a onClick={() =>setStore("windowApp", "Endorsements")}>Endorsements</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "About Me")} class={`hover:cursor-cute-pointer ${store.windowApp == "About Me" ? "text-palette-purple" : ""}`}>About Me</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Resume")} class={`hover:cursor-cute-pointer ${store.windowApp == "Resume" ? "text-palette-purple" : ""}`}>Resume</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Projects")} class={`hover:cursor-cute-pointer ${store.windowApp == "Projects" ? "text-palette-purple" : ""}`}>Projects</a></li>
+                    <li><a onClick={() =>setStore("windowApp", "Endorsements")} class={`hover:cursor-cute-pointer ${store.windowApp == "Endorsements" ? "text-palette-purple" : ""}`}>Endorsements</a></li>
                 </ul>
             </nav>
         </aside>
         <section class="col-span-8 row-span-8 flex flex-col pl-2 pt-2">
-            <Post content="Kevin's a 3rd Year at Cal Poly studying Software Engineering and minoring in the Computing for the Interactive Arts program"/>
-            <Post content="Be sure to check out his other shenanigans and projects! Interests Include: OS, VR Development including 3D Modeling, Web Development"/>
-            <Post content="Look at what others have to say about him! He's sure to provide a valuable asset to your team!"/>
-
+            <Show when={props.mode == "About Me"}>
+                <Post content="Kevin's a 3rd Year at Cal Poly studying Software Engineering and minoring in the Computing for the Interactive Arts program"/>
+                <Post content="Be sure to check out his other shenanigans and projects! Interests Include: OS, VR Development including 3D Modeling, Web Development"/>
+                <Post content="Look at what others have to say about him! He's sure to provide a valuable asset to your team!"/>
+            </Show>
         </section>
 
     </div>);
