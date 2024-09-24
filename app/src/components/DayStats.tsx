0d649eae291ac5b2c700e81332137b8702bdb36d
Kevin <kev.yuan01@gmail.com>
2024-09-24 10:16:37 -0700

    changed font size

--- app/src/components/DayStats.tsx
+++ app/src/components/DayStats.tsx
@@ -2,7 +2,7 @@ export function DayStats(){
 
     const today = new Date();
 
-    return(<section class="border-2 w-[12%] h-3/5 border-l-purple-400 border-t-purple-400 border-b-pink-100 border-r-pink-100 flex items-center justify-center">
-        <span class="text-[100%]">{`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`}</span>
+    return(<section class="border-2 w-full h-3/5 border-l-purple-400 border-t-purple-400 border-b-pink-100 border-r-pink-100 flex items-center justify-center">
+        <span class="text-[1.25vh]">{`${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`}</span>
     </section>)
 }
