3a066cf1c2a4e5d133ece3935b8b083244921881
Kevin <kev.yuan01@gmail.com>
2024-09-24 10:19:11 -0700

    template for display projects

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -1,7 +1,34 @@
-export default function Project(props){
-    return (<div>
-        <slot>
-            
-        </slot>
-    </div>)
+interface IProjectTabs{
+    name: string
+    img: string
+}
+
+export default function ProjectSection(props){
+    return (<div class="h-full w-full overflow-auto">
+        <div class="flex flex-col h-full w-full justify-around items-center">
+            <ProjectTab name="Rizzlet" img=""/>
+            <ProjectTab name="Mealathon" img=""/>
+            <ProjectTab name="And Of Course This Website! Fantastical Dreams" img=""/>
+
+        </div>
+        <div class="h-full w-full">
+            <h1 class="text-2xl">
+                Experience
+            </h1>
+        </div>
+    </div>);
+}
+
+function ProjectTab(props: IProjectTabs){
+    return (
+        <div class="h-1/4 w-full border-2 border-palette-pink">
+            <h1 class="w-full text-2xl border-b-2 border-b-palette-pink pl-2 pr-2 bg-gradient-to-r from-palette-blue to-palette-pink text-palette-purple flex justify-between">
+                {props.name}
+                <div class="aspect-square">
+                    X
+                </div>
+            </h1>
+            <img src={`${props.img}`}/>
+        </div>
+        )
 }
