71505d2622bcf8e2264a62ba0890a8e112243887
Kevin <kev.yuan01@gmail.com>
2024-09-24 19:04:47 -0700

    Removed comments on bottom

--- app/src/components/Posts.tsx
+++ app/src/components/Posts.tsx
@@ -3,14 +3,11 @@ interface IPost{
 }
 
 export default function Post(props: IPost){
-    return (<section class="w-full h-1/3 p-2 flex flex-row mt-5 mb-5 border-2 border-palette-pink bg-gray-200 bg-opacity-10">
+    return (<section class="w-full h-1/3 p-2 flex flex-row mt-5 mb-5 border-r-gray-50 border-b-gray-50 border-[3px] border-palette-pink bg-gray-200 bg-opacity-10 shadow-sm">
         <img src="src/public/images/commenter.png" class="aspect-square h-1/3"/>    
-        <article class="h-full flex-grow flex flex-col ml-2">
-            <span class="w-full h-3/4 text-[1.6vh]">{props.content}</span> 
-            <div class="w-full h-1/3 p-1">
-                <div class="border-2 w-5/6 h-full border-palette-pink"> 
-                    <span class="text-[1.6vh] w-fit h-fit">Comment...</span>
-                </div> 
+        <article class="h-full flex flex-col ml-2 w-full">
+            <span class="h-3/4 text-[1.6vh]">{props.content}</span> 
+            <div class="w-full h-1/4 p-1">
             </div>   
         </article>  
     </section>);
