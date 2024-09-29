9dd32c31da0dbdd961188d4c4398f533cc82261c
Kevin Yuan <kev.yuan01@gmail.com>
2024-09-29 12:39:51 -0700

    changed ui for displaying projects

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -5,28 +5,17 @@ interface IProjectTabs{
 
 export default function ProjectSection(props){
     return (<div class="h-full w-full overflow-auto">
-        <div class="flex flex-col h-full w-full justify-around items-center">
-            <ProjectTab name="Rizzlet" img=""/>
-            <ProjectTab name="Mealathon" img=""/>
-            <ProjectTab name="And Of Course This Website! Fantastical Dreams" img=""/>
-
-        </div>
-        <div class="h-full w-full">
-            <h1 class="text-2xl">
-                Experience
-            </h1>
-        </div>
+        <ProjectTab name="This Website! Fantastical Dreams" img=""/>
+        <ProjectTab name="Rizzlet" img=""/>
+        <ProjectTab name="Mealathon" img=""/>
     </div>);
 }
 
 function ProjectTab(props: IProjectTabs){
     return (
-        <div class="h-1/4 w-full border-2 border-palette-pink">
-            <h1 class="w-full text-2xl border-b-2 border-b-palette-pink pl-2 pr-2 bg-gradient-to-r from-palette-blue to-palette-pink text-palette-purple flex justify-between">
-                {props.name}
-                <div class="aspect-square">
-                    X
-                </div>
+        <div class="h-full w-full border-2 border-palette-pink mb-2">
+            <h1 class="w-full text-[1.8vh] border-b-2 border-b-palette-pink pl-2 pr-2 bg-gradient-to-r from-palette-blue to-palette-pink text-palette-purple flex justify-between">
+                {props.name} 
             </h1>
             <img src={`${props.img}`}/>
         </div>
