83b894e0cbabc72e219d1c4e2583a763ffa7c930
Kevin <kev.yuan01@gmail.com>
2024-10-02 00:06:05 -0700

    Reformated posts

--- app/src/components/Posts.tsx
+++ app/src/components/Posts.tsx
@@ -1,14 +1,18 @@
+import { JSX } from "solid-js";
+
 interface IPost{
-    content: string;
+    content: JSX.Element;
+    title: string;
 }
 
 export default function Post(props: IPost){
-    return (<section class="w-full h-1/3 p-2 flex flex-row mt-5 mb-5 border-r-gray-50 border-b-gray-50 border-[3px] border-palette-pink bg-gray-200 bg-opacity-10 shadow-sm">
-        <img src="src/public/images/commenter.png" class="aspect-square h-1/3"/>    
-        <article class="h-full flex flex-col ml-2 w-full">
-            <span class="h-3/4 text-[1.6vh]">{props.content}</span> 
-            <div class="w-full h-1/4 p-1">
-            </div>   
+    return (<section class="w-full h-1/2 p-2 flex flex-row mt-5 mb-5 border-r-gray-50 border-b-gray-50 border-[3px] border-palette-pink bg-gray-200 bg-opacity-10 shadow-sm">
+        <img src="src/public/images/commenter.png" class="aspect-square h-1/5"/>    
+        <article class="h-full flex flex-col ml-2 w-full text-[1.5vh]">
+            <div class="h-[10%] bg-gradient-to-r from-palette-blue to-palette-pink text-palette-purple">
+                {props.title}
+            </div>
+            {props.content}
         </article>  
     </section>);
 }                   
