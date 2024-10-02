ea6ab7d1e1ccac672757c240776fd3e5b24bfbe9
Kevin <kev.yuan01@gmail.com>
2024-10-01 23:08:59 -0700

    readjusted url

--- app/src/components/AppScreen.tsx
+++ app/src/components/AppScreen.tsx
@@ -33,7 +33,7 @@ function AppScreen(props){
                     <img src="src/public/images/left_web_button.png" class={`h-1/2 m-1 ${store.currentPage <= 0 ? 'grayscale pointer-events-none' : "cursor-cute-pointer"}`} onClick={() => traverseHistory(-1)}/>
                     <img src="src/public/images/right_web_button.png" class={`h-1/2 m-1 ${store.currentPage == store.searchHistory.length - 1 ? 'grayscale pointer-events-none' : "cursor-cute-pointer"}`} onClick={() => traverseHistory(1)}/>
                     <div class="flex-grow h-1/2 bg-white border-2 border-b-gray-50 border-t-gray-300 border-l-gray-300 border-r-gray-50">
-                        <span>{`https://${store.windowApp.replace(" ", "")}.com`}</span>
+                        <span class="ml-1 h-full flex items-center">{`https://${store.windowApp.replace(" ", "")}.com`}</span>
                     </div>
                 </div>
                 <section class="border-gray-200 border-r-gray-50 border-b-gray-50 border-4 border-solid w-full flex-grow h-[89%]">
